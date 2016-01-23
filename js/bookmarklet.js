7470b637d5ba4e9b07c321ce9ffe041d05789e83
Olaf Peters <olf@magicolf.de>
2016-01-23 20:54:41 +0100

    modify bookmarklet to point to github URL

--- js/bookmarklet.js
+++ js/bookmarklet.js
@@ -1,11 +1,11 @@
 javascript:void(function(){
-    var u = "http://localhost:3000/";
+    var u = "http://olf.github.io/mhmapsolver/";
     var l = document.querySelectorAll('.treasureMapPopup-mice-groups.uncaughtmice .treasureMapPopup-mice-group-mouse-name span');
     var m = "";
     for (i=0; i<l.length;i++) {
         m += l[i].textContent.replace(" ", "+") + (i==l.length-1 ? '' : '/');
     }
     if (l.length > 0) {
-        window.open(u + "?mice=" + m,'_blank');
+        window.open(u + "?mice=" + m,'mhmapsolver');
     }
 })()
